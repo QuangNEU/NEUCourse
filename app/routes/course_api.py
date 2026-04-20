542895bdac27c59d6640ae8cef09662302f1a8d3
LuuHieu <luuminhhieu1107@gmail.com>
2026-04-21 00:51:27 +0700

    fix: resolve conflict (#8)
    * feat: update apis and ui for user functions
    
    * fix: resolve conflict

--- app/routes/course_api.py
+++ app/routes/course_api.py
@@ -7,7 +7,6 @@ from ..models import (
     db, Truong, KhoaVien, NganhHoc, PhienBanCT, HocPhan, KhungChuongTrinh,
     DeCuongChiTiet, ChuanDauRa, KeHoachGiangDay, DanhGiaHocPhan, HocLieu, User
 )
-import os
 import google.generativeai as genai
 from dotenv import load_dotenv
 
@@ -395,7 +394,6 @@ def faculty_detail(id):
 
 @course_bp.route('/major/<int:id>')
 def major_detail(id):
-    # Lấy thông tin ngành
     major = NganhHoc.query.get_or_404(id)
     version_code = request.args.get('version', '')
     version, versions = _pick_version(id, version_code)
@@ -584,28 +582,6 @@ def major_pdf(id):
     return send_file(pdf_buffer, as_attachment=True, download_name=filename, mimetype='application/pdf')
 
 
-    # Lấy phiên bản chương trình đào tạo
-    version = PhienBanCT.query.filter_by(nganh_id=id).first()
-
-    if not version:
-        return "Chưa có khung chương trình cho ngành này", 404
-
-    # Lấy danh sách khung chương trình
-    curriculum_list = KhungChuongTrinh.query.filter_by(phien_ban_id=version.id) \
-        .order_by(KhungChuongTrinh.hoc_ky_du_kien.asc()) \
-        .all()
-
-    # Tính tổng số tín chỉ
-    total_credits = sum(item.hoc_phan.so_tin_chi for item in curriculum_list)
-
-    # Đổi tên file giao diện thành major_detail.html cho đồng bộ
-    return render_template(
-        'major_detail.html',
-        major=major,
-        version=version,
-        curriculum_list=curriculum_list,
-        total_credits=total_credits
-    )
 @course_bp.route('/course/<int:id>')
 def course_detail(id):
     course = HocPhan.query.get_or_404(id)
